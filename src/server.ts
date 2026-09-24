c713f44cd351d9bdfc5f9bf5edaf9687f5f5e497
Tawsif Hossain <tawsifhossain18.o@gmail.com>
2026-09-24 14:42:25 +0600

    Added Nodemailer and pass change

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
 import app from "./app";
 import config from "./app/config";
+import { transporter } from "./app/lib/nodemailer";
+
 import { prisma } from "./app/lib/prisma";
+import { redisClient } from "./app/lib/redis";
+import { seedSuperAdmin, seedTesterAdmin, seedTesterDoctor } from "./app/utils/seed";
 
 const PORT = config.port;
 
@@ -8,6 +12,17 @@ const main = async () => {
 	try {
 		await prisma.$connect();
 		console.log("Connected to the database successfully.");
+
+		await redisClient.connect();
+		console.log("Redis Connected Successfully.");
+
+		await transporter.verify();
+		console.log("Nodemailer Connected Successfully.");
+		
+		await seedSuperAdmin();
+		await seedTesterAdmin();
+		await seedTesterDoctor();
+		
 		app.listen(PORT, () => {
 			console.log(`Server is running on port ${PORT}`);
 		});
